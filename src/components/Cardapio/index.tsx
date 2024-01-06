f16f7ad6147417cb1d8741b2bcb1472cd3e69ae8
Leandro Pimentel <leandropimentel2011@gmail.com>
2024-01-06 00:33:56 -0300

    fix: alterado cardapio

--- src/components/Cardapio/index.tsx
+++ src/components/Cardapio/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react"
 export const DownloadCardapio = () => {
     useEffect(() => {
         // Redireciona para a nova URL quando a página é carregada
-        window.location.href = 'https://drive.google.com/file/d/15uFEFK0k2sS1EvJoViJagIPMev9JAqGO/view';
+        window.location.href = 'https://drive.google.com/file/d/1z244aJ4neni8I9v8rE8qlT05eYHNrVLj/view';
       }, []);
     return (
         <div className="w-full h-screen flex items-center justify-center">
