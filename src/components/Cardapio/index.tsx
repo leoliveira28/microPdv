042b6a6fc99d92a7436acf3fef18624e8cb734e0
Leandro Pimentel <leandropimentel2011@gmail.com>
2024-01-12 18:30:53 -0300

    fix: alaterado cardapio

--- src/components/Cardapio/index.tsx
+++ src/components/Cardapio/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react"
 export const DownloadCardapio = () => {
     useEffect(() => {
         // Redireciona para a nova URL quando a página é carregada
-        window.location.href = 'https://drive.google.com/file/d/1z244aJ4neni8I9v8rE8qlT05eYHNrVLj/view';
+        window.location.href = 'https://drive.google.com/file/d/1cRZFqxzXDO-GCqfEL6M3ExvStaKNFJnL/view?usp=sharing';
       }, []);
     return (
         <div className="w-full h-screen flex items-center justify-center">
